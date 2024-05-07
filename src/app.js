09108827ded410b2cf24c052a831bfee2de24ed2
Surendar-Kailasam <163517265+Surendar-Kailasam@users.noreply.github.com>
2024-05-07 07:06:04 +0000

    home page is built

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const path = require('path');
 
 const app = express();
 
+app.use(express.static(path.join(__dirname,'public')));
+
 app.get ('/',(req,res)=>{
     // res.send('Hello World');
     res.sendFile(path.join(__dirname,'pages/index.html'));
