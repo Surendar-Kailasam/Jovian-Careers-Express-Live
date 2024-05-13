42b31d36308a90d546addb97956a4fe34ff0f1fe
Surendar-Kailasam <163517265+Surendar-Kailasam@users.noreply.github.com>
2024-05-13 06:29:56 +0000

    Complete upto application form creation

--- src/app.js
+++ src/app.js
@@ -1,14 +1,34 @@
 const express = require('express');
 const path = require('path');
 
+const JOBS= require('./jobs');
+const mustacheExpress=require('mustache-express');
+
+
 const app = express();
 
 app.use(express.static(path.join(__dirname,'public')));
 
+app.set ('views',path.join(__dirname ,'pages'));
+app.set ('view engine', 'mustache');
+app.engine ('mustache', mustacheExpress());
+
 app.get ('/',(req,res)=>{
     // res.send('Hello World');
-    res.sendFile(path.join(__dirname,'pages/index.html'));
+    // res.sendFile(path.join(__dirname,'pages/index.html'));
+    res.render('index',{jobs: JOBS});
 });
+
+app.get('/jobs/:id', (req,res) => {
+    const id = req.params.id;
+    const matchedJob = JOBS.find(job => job.id.toString() === id );
+    console.log('matchedJob',matchedJob);
+    res.render('job',{job : matchedJob});
+});
+
+app.post('/jobs/:id/apply',(req,res) => {
+    res.send('Got the application');
+})
 
 const port = process.env.port || 3000;
 
